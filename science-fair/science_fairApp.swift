476fc50c193cbec64072a4427cb1812b97d566a2
hongyi <liuhongyi790@gmail.com>
2022-12-08 11:13:25 +0800

    Initial Commit

---
+++ science-fair/science_fairApp.swift
@@ -0,0 +1,17 @@
+//
+//  science_fairApp.swift
+//  science-fair
+//
+//  Created by liufamily on 2022/12/8.
+//
+
+import SwiftUI
+
+@main
+struct science_fairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
